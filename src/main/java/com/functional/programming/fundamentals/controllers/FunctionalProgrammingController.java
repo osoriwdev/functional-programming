32012850a7bdf91827431dce39524c3f2a9982f2
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2024-08-11 19:42:47 -0500

    Creating new FunctionalProgrammingController class
    Creating FunctionalProgrammingController  Controller and implementing endpoint /functionalInterface/function/{param}

---
+++ src/main/java/com/functional/programming/fundamentals/controllers/FunctionalProgrammingController.java
@@ -0,0 +1,30 @@
+package com.functional.programming.fundamentals.controllers;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.function.Function;
+
+@RestController
+@RequestMapping("/functionalProgramming")
+public class FunctionalProgrammingController {
+
+    /**
+     * Uses a functional function interface to obtain lowercase from a string passed to as a path variable.
+     *
+     * @param param the string
+     * @return the string in lowercase
+     */
+    @GetMapping("/functionalInterface/function/{param}")
+    public ResponseEntity<String> useFunctionInterface(@PathVariable("param") String parameter) {
+        Function<String, String> function = String::toLowerCase;
+        return new ResponseEntity<>(function.apply(parameter), HttpStatus.OK);
+    }
+
+}
